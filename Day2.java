51aefb7adfcdf09abc705779808fd16f0cb7ee2e
tanayakulkarni <45399057+tanayakulkarni@users.noreply.github.com>
2018-11-29 12:31:34 -0500

    Update Day2.java
    Finished, pt 1

--- Day2.java
+++ Day2.java
@@ -1,15 +1,31 @@
 import java.util.ArrayList;
 public class Day2{
-	public static int convertToArrayList(int num){
-	return 1;
+	public static ArrayList<Integer>  convertToArrayList(int num){
+	ArrayList<Integer> list2 = new ArrayList<>();
+	int count=0;
+	int c= num;
+	while (c>1){
+		c/=10;
+		count++;
+	}
+	for(int i=0; i<count; i++){
+		list2.add(0);
+	}
+	while(num>1){
+		list2.set(count-1,num%10);
+		count--;
+		num/=10;
 	}
 
+	return list2;
+	}
 	public static ArrayList<Integer> convertToInt(ArrayList<Integer> list1){
 	return list1;
 	}
 
 	public static void main(String args[]){
-	int num;
+	int num= 8950;
 	ArrayList<Integer> list = new ArrayList<>();
-	}
+	System.out.println(convertToArrayList(num));
+}
 }
